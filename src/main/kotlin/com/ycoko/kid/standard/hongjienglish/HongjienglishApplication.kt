b28194745e40546c0c25f7af2702cb3ce02e8dff
a5365958 <2571706435@qq.com>
2020-12-19 20:11:21 +0800

    init

---
+++ src/main/kotlin/com/ycoko/kid/standard/hongjienglish/HongjienglishApplication.kt
@@ -0,0 +1,11 @@
+package com.ycoko.kid.standard.hongjienglish
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HongjienglishApplication
+
+fun main(args: Array<String>) {
+	runApplication<HongjienglishApplication>(*args)
+}
---
+++ src/test/kotlin/com/ycoko/kid/standard/hongjienglish/HongjienglishApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ycoko.kid.standard.hongjienglish
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HongjienglishApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
