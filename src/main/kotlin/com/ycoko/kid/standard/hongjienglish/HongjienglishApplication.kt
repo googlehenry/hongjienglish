8c6ad737852b61696793b0d02f2d020409302bd3
a5365958 <2571706435@qq.com>
2020-12-20 12:04:56 +0800

    add page

--- src/main/kotlin/com/ycoko/kid/standard/hongjienglish/HongjienglishApplication.kt
+++ src/main/kotlin/com/ycoko/kid/standard/hongjienglish/HongjienglishApplication.kt
@@ -3,7 +3,7 @@ package com.ycoko.kid.standard.hongjienglish
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication()
+@SpringBootApplication
 class HongjienglishApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/ycoko/kid/standard/hongjienglish/entity/LearnItem.kt
+++ src/main/kotlin/com/ycoko/kid/standard/hongjienglish/entity/LearnItem.kt
@@ -1,5 +1,6 @@
 package com.ycoko.kid.standard.hongjienglish.entity
 
+import java.util.*
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.Id
@@ -7,13 +8,27 @@ import javax.persistence.Table
 
 
 @Entity
-@Table(name = "LEARN_ITEM")
+@Table(name = "learn_item")
 class LearnItem(
         @Id
-        @Column(name = "ID")
+        @Column(name = "id")
         var id: String,
-        @Column(name = "GRADE")
+        @Column(name = "grade")//1-12
         var grade: Int,
-        @Column(name = "type")
-        var type: String
+        @Column(name = "category")//amimal
+        var category: String,
+        @Column(name = "name")//apple,苹果
+        var name: String,
+        @Column(name = "locale")//en_us,zh_cn
+        var locale: String,
+        @Column(name = "image")
+        var image: String,
+        @Column(name = "audio")
+        var audio: String,
+        @Column(name = "video")
+        var video: String,
+        @Column(name = "description")
+        var description: String
+
+
 )
